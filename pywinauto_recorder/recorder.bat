3918f38a245db40ba747ff887f4840553314cb20
David Pratmarty <david.pratmarty@oktal-se.fr>
2020-02-26 13:44:47 +0100

    More explanations

--- pywinauto_recorder/recorder.bat
+++ pywinauto_recorder/recorder.bat
@@ -1,6 +1,6 @@
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 
-..\Python\Pyportable-2.7.10rc1\python.exe recorder.py 
+D:\Compil\workbench-2019-git\Software\AutomatedValidation\SE-VALIDATION\Python\Pyportable-2.7.10rc1\python.exe recorder.py 
 
 
 exit 
