3038ea31e85c68bec38700dd8161824aff8f3d42
David Pratmarty <david.pratmarty@oktal-se.fr>
2020-03-16 14:59:48 +0100

    Refactoring

---
+++ pywinauto_recorder/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Pywinauto recorder records user interface actions and saves them in a Python script.
+    The generated Python script plays back user interface actions in the order in which the user recorded them.
+
+    Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
+"""
+
+__version__ = "0.0.1"
+
+from recorder import Recorder
+from player import *
