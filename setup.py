909ebb91f075d12837cc49d2f1deaa39c9bac87f
David Pratmarty <d_pratmarty@hotmail.com>
2020-05-20 09:40:34 +0200

    Python 3.8 is now supported

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 import pywinauto_recorder
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pywinauto_recorder',
     version=pywinauto_recorder.__version__,
@@ -10,5 +13,22 @@ setup(
     license='MIT',
     author='david pratmarty',
     author_email='david.pratmarty@gmail.com',
-    description='pywinauto recorder', install_requires=['pywinauto', 'keyboard', 'mouse', 'overlay_arrows_and_more']
+    description='pywinauto recorder',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    install_requires=['pywinauto', 'keyboard', 'mouse', 'overlay_arrows_and_more'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Testing',
+    ]
 )
