6e619d5b3a5628cfe326ace08ff0079ff7a1364c
David Pratmarty <d_pratmarty@hotmail.com>
2020-05-21 13:34:37 +0200

    Splash screen update

--- pywinauto_recorder/pywinauto_recorder.py
+++ pywinauto_recorder/pywinauto_recorder.py
@@ -15,29 +15,45 @@ from player import *
 
 def display_splash_screen():
 	splash_screen = oaam.Overlay(transparency=0.0)
+	time.sleep(0.2)
+	splash_screen2 = oaam.Overlay(transparency=0.1)
+	splash_screen3 = oaam.Overlay(transparency=0.1)
 	screen_width = GetSystemMetrics(0)
 	screen_height = GetSystemMetrics(1)
 	nb_band = 15
 	line_height = (screen_height / 2) / nb_band
 	text_lines = [''] * nb_band
-	text_lines[1] = 'Pywinauto recorder'
-	text_lines[2] = 'version ' + __version__
+	text_lines[1] = 'Pywinauto recorder version ' + __version__
+	text_lines[2] = 'by David Pratmarty'
 	text_lines[4] = 'CTRL+ALT+R : Recording/Pause'
 	text_lines[6] = 'Search algorithm speed'
 	text_lines[8] = 'CTRL+SHIFT+F : Copy element path in clipboard'
 	text_lines[10] = 'CTRL+ALT+Q : Quit'
 	text_lines[12] = 'Drag and drop a recorded file on '
 	text_lines[13] = 'pywinauto_recorder.exe to replay it'
-	for n in xrange(5):
+
+	splash_screen2.clear_all()
+	splash_screen3.clear_all()
+	x, y, w, h = screen_width / 3, screen_height / 4, screen_width / 3, line_height * nb_band
+	splash_screen2.add(
+		geometry=oaam.Shape.triangle, thickness=0,
+		xyrgb_array=((x, y, 200, 0, 0), (x+w, y, 0, 128, 0), (x+w, y+h, 0, 0, 155)))
+
+	splash_screen3.add(
+		geometry=oaam.Shape.triangle, thickness=0,
+		xyrgb_array=((x+w+1, y+h, 0, 0, 155), (x, y+h, 22, 128, 66), (x+1, y-1, 200, 0, 0)))
+
+	splash_screen2.refresh()
+	splash_screen3.refresh()
+
+	for n in range(5):
 		splash_screen.clear_all()
-		color = [60, 70, 90]
 		i = 0
 		while i < nb_band:
 			splash_screen.add(
-				x=screen_width / 3 - i*8, y=screen_height / 4 + i * line_height, width=screen_width / 3 + i*8*2,
-				height=line_height, text=text_lines[i], text_color=(0, 0, 0), color=tuple(color),
-				geometry=oaam.Shape.rectangle, thickness=1, brush=oaam.Brush.solid, brush_color=tuple(color))
-			color[0], color[1], color[2] = color[0] + 2, color[1] + 1, color[2] + 3
+				x=screen_width / 3, y=screen_height / 4 + i * line_height, width=screen_width / 3,
+				height=line_height, text=text_lines[i], text_color=(254, 255, 255), color=(255, 255, 255),
+				geometry=oaam.Shape.rectangle, thickness=1 )
 			i = i + 1
 		if n % 2 == 0:
 			overlay_add_record_icon(
@@ -53,6 +69,10 @@ def display_splash_screen():
 		time.sleep(0.8)
 	splash_screen.clear_all()
 	splash_screen.refresh()
+	splash_screen2.clear_all()
+	splash_screen2.refresh()
+	splash_screen3.clear_all()
+	splash_screen3.refresh()
 
 
 if __name__ == '__main__':
