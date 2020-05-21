591f84b3594d8daa320521930aa68c3815ce4ec3
David Pratmarty <d_pratmarty@hotmail.com>
2020-05-21 13:34:37 +0200

    Splash screen update

--- pywinauto_recorder/__init__.py
+++ pywinauto_recorder/__init__.py
@@ -8,7 +8,7 @@
     Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
 """
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 
 from recorder import Recorder
 from player import *
