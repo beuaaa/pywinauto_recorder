ecac7c9fbfacb418c66dd9b8522df1714402a88f
David Pratmarty <d_pratmarty@hotmail.com>
2020-06-30 21:43:08 +0200

    Install_requires and requirements.txt

--- pywinauto_recorder/__init__.py
+++ pywinauto_recorder/__init__.py
@@ -10,7 +10,7 @@
     Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
 """
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 from .player import *
 from .recorder import Recorder
