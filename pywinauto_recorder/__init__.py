1a4235b285fe9c5f322420d5c065d418f0933921
David Pratmarty <david.pratmarty@gmail.com>
2020-12-26 17:18:33 +0100

    Splash screen displays correctly in low resolution

--- pywinauto_recorder/__init__.py
+++ pywinauto_recorder/__init__.py
@@ -10,7 +10,7 @@
     Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .player import *
 from .recorder import Recorder
