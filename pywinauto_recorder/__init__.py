249fdcc39086db8006eed4545bee15a793b3325a
David Pratmarty <david.pratmarty@gmail.com>
2021-02-16 16:21:38 +0100

    Added Pywinauto recorder tray icon

--- pywinauto_recorder/__init__.py
+++ pywinauto_recorder/__init__.py
@@ -10,7 +10,7 @@
     Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
 """
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 from .player import *
 from .recorder import Recorder
