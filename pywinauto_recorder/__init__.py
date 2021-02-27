e4b843e4e339b82dcfd409cab29643f5ab70f087
David Pratmarty <david.pratmarty@gmail.com>
2021-02-27 14:33:41 +0100

    Added docstring to player.py

--- pywinauto_recorder/__init__.py
+++ pywinauto_recorder/__init__.py
@@ -10,7 +10,7 @@
     Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .player import *
 from .recorder import Recorder
