5a3637e8a0af993593d54172a6b696a18cad40c2
David Pratmarty <david.pratmarty@gmail.com>
2021-02-28 12:27:16 +0100

    Added autogeneration of docstring documentation

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,12 @@ sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../pywinauto_recorder/'))
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', ]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinxcontrib.apidoc',]
+
+apidoc_module_dir = '../pywinauto_recorder'
+apidoc_output_dir = 'reference/api'
+apidoc_excluded_paths = ['tests']
+apidoc_separate_modules = True
 
 autosummary_generate = True
 
