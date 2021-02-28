ea7ec30b9045f5e88cc34aa796f23333a8a35788
David Pratmarty <david.pratmarty@gmail.com>
2021-02-28 12:05:33 +0100

    Added autogeneration of docstring documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../pywinauto_recorder'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../pywinauto_recorder/'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', ]
 
