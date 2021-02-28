349f18c39b2a661c25ae96bf7021a0d58294efa9
David Pratmarty <david.pratmarty@gmail.com>
2021-02-28 12:41:58 +0100

    Added autogeneration of docstring documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../pywinauto_recorder/'))
+sys.path.insert(0, os.path.abspath('..'))
 
-print(sys.path)
+
+import pywinauto_recorder
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', ]
 
@@ -16,5 +15,12 @@ autosummary_generate = True
 
 templates_path = ['_templates']
 
+source_suffix = '.rst'
+
 master_doc = 'index'
 
+
+# General information about the project.
+project = u'pywinauto_recorder'
+copyright = u"2021, David Pratmarty"
+author = u"David Pratmarty"
