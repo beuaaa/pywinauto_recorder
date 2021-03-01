0244489d93fc4dc78cf41e810227be275d129b7d
David Pratmarty <david.pratmarty@gmail.com>
2021-03-01 13:22:00 +0100

    Added shortcut CTRL+SHIFT+D to switch between Info and Stop mode

--- pywinauto_recorder/__init__.py
+++ pywinauto_recorder/__init__.py
@@ -10,7 +10,7 @@
     Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
 """
 
-__version__ = "0.3.1"
+__version__ = "0.3.3"
 
 from .player import *
 from .recorder import Recorder
