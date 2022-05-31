40386875aca6a1a1fe511f3e02a3f0bf0798d4f0
David Pratmarty <d_pratmarty@hotmail.com>
2022-05-31 15:15:48 +0200

    This commit quality was not up to standards. It is fixed now.

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-from pywinauto_recorder.core import *
+from pywinauto_recorder.core import get_entry_list, get_entry, match_entry_list
 
 
 ################################################################################################
@@ -9,7 +8,9 @@ from pywinauto_recorder.core import *
 ################################################################################################
 
 def test_get_entry_list():
-	""" Tests get_entry_list(element_path) """
+	"""
+	Tests get_entry_list(element_path).
+	"""
 	element_path = "D:\\Name||Window->||Pane->Property||Group->"
 	element_path = element_path + "Label:||Text#[Name||Window->||Pane->Property||Group->Label:||Text,0]%(0,0)"
 	entry_list = get_entry_list(element_path)
@@ -20,7 +21,9 @@ def test_get_entry_list():
 
 
 def test_get_entry_list_with_asterisk():
-	""" Tests get_entry_list(element_path) element_path has some asterisks"""
+	"""
+	Tests get_entry_list(element_path) having element_path with some asterisks.
+	"""
 	element_path = "Name||Window->*->||Group->Property||%(0,0)"
 	entry_list2 = get_entry_list(element_path)
 	assert entry_list2[1] == '*'
@@ -29,50 +32,45 @@ def test_get_entry_list_with_asterisk():
 
 
 def test_get_entry_elements():
-	""" Tests get_entry(entry) """
-	entry_list = [
-		'Name:||Type#[0,0]%(2,-24)', '||Type#[0,0]%(2,-24)', 'Name:||#[0,0]%(2,-24)',
-		'Name:||Type#[0,0]', '||Type#[0,0]', 'Name:||#[0,0]',
-		'Name:||Type', '||Type', 'Name:||',
-		'Name||Type', '||Type', 'Name||',
-		'Name||Type#[y_name:||y_type,0]', '||Type#[y_name:||y_type,0]', 'Name||#[y_name:||y_type,0]',
-		'Name||Type#[y_name||y_type,0]', '||Type#[y_name||y_type,0]', 'Name||#[y_name||y_type,0]',
+	"""
+	Tests get_entry(entry).
+	"""
+	str_name_with_colon = 'Name:'
+	str_name_without_colon = 'Name'
+	str_y_x_with_colon = 'y_name:||y_type'
+	str_y_x_without_colon = 'y_name||y_type'
+	test_data = [
+		('Name:||Type#[0,0]%(2,-24)',      (str_name_with_colon, 'Type', [0,0], (2,-24))),
+		('||Type#[0,0]%(2,-24)',           ('', 'Type', [0,0], (2,-24))),
+		('Name:||#[0,0]%(2,-24)',          (str_name_with_colon, None, [0,0], (2,-24))),
+		('Name:||Type#[0,0]',              (str_name_with_colon, 'Type', [0,0], None)),
+		('||Type#[0,0]',                   ('', 'Type', [0,0], None)),
+		('Name:||#[0,0]',                  (str_name_with_colon, None, [0,0], None)),
+		('Name:||Type',                    (str_name_with_colon, 'Type', None, None)),
+		('||Type',                         ('', 'Type', None, None)),
+		('Name:||',                        (str_name_with_colon, None, None, None)),
+		('Name||Type',                     (str_name_without_colon, 'Type', None, None)),
+		('||Type',                         ('', 'Type', None, None)),
+		('Name||',                         (str_name_without_colon, None, None, None)),
+		('Name||Type#[y_name:||y_type,0]', (str_name_without_colon, 'Type', [str_y_x_with_colon, 0], None)),
+		('||Type#[y_name:||y_type,0]',     ('', 'Type', [str_y_x_with_colon, 0], None)),
+		('Name||#[y_name:||y_type,0]',     (str_name_without_colon, None, [str_y_x_with_colon, 0], None)),
+		('Name||Type#[y_name||y_type,0]',  (str_name_without_colon, 'Type', [str_y_x_without_colon, 0], None)),
+		('||Type#[y_name||y_type,0]',      ('', 'Type', [str_y_x_without_colon, 0], None)),
+		('Name||#[y_name||y_type,0]',      (str_name_without_colon, None, [str_y_x_without_colon, 0], None))
 	]
-
-	for i, entry in enumerate(entry_list):
-		str_name, str_type, y_x, dx_dy = get_entry(entry)
-		if i % 3 == 0:
-			if i < 9:
-				assert str_name == 'Name:'
-			else:
-				assert str_name == 'Name'
-			assert str_type == 'Type'
-		if i % 3 == 1:
-			assert str_name == ''
-			assert str_type == 'Type'
-		if i % 3 == 2:
-			if i < 9:
-				assert str_name == 'Name:'
-			else:
-				assert str_name == 'Name'
-			assert str_type is None
-		if i < 3:
-			assert dx_dy == (2, -24)
-		else:
-			assert dx_dy is None
-		if i < 6:
-			assert y_x == [0, 0]
-		elif i >= 15:
-			assert y_x == ['y_name||y_type', 0]
-		elif i >= 12:
-			assert y_x == ['y_name:||y_type', 0]
-		else:
-			assert y_x is None
+	for data in test_data:
+		str_name, str_type, y_x, dx_dy = get_entry(data[0])
+		assert str_name == data[1][0]
+		assert str_type == data[1][1]
+		assert y_x == data[1][2]
+		assert dx_dy == data[1][3]
 
 
 def test_match_entry_list():
-	""" Tests match_entry_list(entry_list, template_list, regex_title=False) """
-
+	"""
+	Tests if the entry_list matches the template_list.
+	"""
 	template_list = get_entry_list("wName||Window->*->Tab1||Tab->A||Group->B||Group->*->Login||Button")
 	entry_path_set = [
 		("wName||Window->Tab1||Tab->A||Group->B||Group->Login||Button", True),
@@ -81,7 +79,6 @@ def test_match_entry_list():
 		("wName||Window->bla||bla->Tab1||Tab->bla||bla->A||Group->B||Group->bla||bla->Login||Button", False),
 		("wName||Window->bla||bla->bla||bla->Tab1||Tab->A||Group->B||Group->bla||bla->bla||bla->Login||Button", True),
 	]
-
 	for entry_path in entry_path_set:
 		assert match_entry_list(get_entry_list(entry_path[0]), template_list) == entry_path[1]
 
