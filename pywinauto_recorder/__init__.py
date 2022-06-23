e1eb99a7ffcfd76d3042046268eb831f7ee1659a
David Pratmarty <d_pratmarty@hotmail.com>
2022-06-23 16:36:48 +0200

    Pywinauto recorder 0.6.2 compiled with Nuitka 0.8.4 and Python 3.8.

--- pywinauto_recorder/__init__.py
+++ pywinauto_recorder/__init__.py
@@ -10,7 +10,7 @@
     Pywinauto recorder uses accessibility technologies via the Pywinauto_ library.
 """
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 from .player import *
 from .recorder import Recorder
